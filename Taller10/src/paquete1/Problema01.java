ac5da73c6a6d0f202061713e2c79f5f5ac3aabb8
Mxteo2008 <mateo.jimenez.bvm@gmail.com>
2025-12-18 10:58:46 -0500

    problema 1

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -1,20 +1,54 @@
-/*
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete1;
 
-/**
- *
- * @author reroes
- */
 public class Problema01 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        boolean[][] asistencia = {
+            {true, true, false, true, true},
+            {true, false, false, true, false},
+            {true, true, true, true, true},
+            {false, true, true, false, true}
+        };
+
+        String[] estudiantes = {"Ana", "Luis", "Carlos", "María"};
+
+        int[] totalAsistencias = new int[4];
+
+        String reporte = "";
+        String estudiantesCompletos = "";
+
+        for (int i = 0; i < asistencia.length; i++) {
+            int sumaAsistencia = 0;
+
+            for (int j = 0; j < asistencia[i].length; j++) {
+                if (asistencia[i][j]) {
+                    sumaAsistencia = sumaAsistencia + 1;
+                }
+            }
+            totalAsistencias[i] = sumaAsistencia;
+        }
+        for (int i = 0; i < estudiantes.length; i++) {
+
+            reporte = String.format("%sEstudiante: %s\nAsistencias: %d\n",
+                    reporte,
+                    estudiantes[i],
+                    totalAsistencias[i]);
+
+            if (totalAsistencias[i] == 5) {
+                reporte = String.format("%sAsistencia completa\n\n", reporte);
+                estudiantesCompletos = String.format("%s%s\n", 
+                        estudiantesCompletos, estudiantes[i]);
+            } else {
+                reporte = String.format("%sNo tiene asistencia completa\n\n", 
+                        reporte);
+            }
+        }
+ 
+        reporte = String.format("%sEstudiantes con asistencia completa:\n%s",
+                reporte,
+                estudiantesCompletos);
+
+        System.out.println(reporte);
     }
-    
 }
