b9e72b7884550cd7d702efc127d9add8b58db788
Mxteo2008 <mateo.jimenez.bvm@gmail.com>
2025-12-18 11:13:47 -0500

    Problema 2

--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -10,5 +10,41 @@ package paquete2;
  * @author reroes
  */
 public class Problema02 {
-    
+
+    public static void main(String[] args) {
+
+        int[][] edades = {
+            {5, 12, 35, 40},
+            {8, 15, 33, 38},
+            {6, 10, 30, 36}
+        };
+        int[] adultos = new int[edades.length];
+        int menorEdad = 0;
+        String mensaje = "";
+        for (int i = 0; i < edades.length; i++) {
+            int adultosNum = 0;
+
+            for (int j = 0; j < edades[i].length; j++) {
+                if (edades[i][j] < 18) {
+                    menorEdad = menorEdad + 1;
+
+                } else {
+                    adultosNum = adultosNum + 1;
+
+                }
+                adultos[i] = adultosNum;
+            }
+        }
+
+        for (int i = 0; i < adultos.length; i++) {
+            mensaje = String.format("%sFamilia numero %s\nAdultos en total: %s\n",
+                    mensaje,
+                    i + 1,
+                    adultos[i]);
+
+        }
+        mensaje = String.format("%sMenores de edad en total: %s", mensaje,
+                menorEdad);
+        System.out.println(mensaje);
+    }
 }
