b476178d0a2a107c886a3925b3e95eac6689441c
Mxteo2008 <mateo.jimenez.bvm@gmail.com>
2025-12-18 11:40:58 -0500

    problema 3

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete3;
 
-/**
- *
- * @author reroes
- */
 public class Problema03 {
-    
+
+    public static void main(String[] args) {
+
+        boolean[][] sensores = {
+            {true, true, true, true, true, true},
+            {true, false, true, false, true, false},
+            {false, false, false, false, false, false},
+            {true, true, false, true, true, false}
+        };
+
+        int zonasF = 0;
+        String reporte = "";
+
+        for (int i = 0; i < sensores.length; i++) {
+            boolean sinFallos = true;
+
+            for (int j = 0; j < sensores[i].length; j++) {
+                if (!sensores[i][j]) {
+                    sinFallos = false;
+                }
+            }
+            if (sinFallos) {
+                zonasF++;
+                reporte = String.format("%sZona %d: Funciona sin fallos\n",
+                        reporte, (i + 1));
+            } else {
+                reporte = String.format("%sZona %d: Presenta al "
+                        + "menos un fallo\n",
+                        reporte, (i + 1));
+            }
+        }
+
+        reporte = String.format("%s\nTotal de zonas sin fallos: %d",
+                reporte, zonasF);
+
+        System.out.println(reporte);
+
+    }
 }
